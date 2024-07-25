d9cffc70ba40c4ab0300103c606ae20facce85c8
soheil <soheil.amiri@Live.com>
2024-07-25 13:45:39 +0330

    testbackup

--- Powershell/windows/checkbackupstatus.ps1
+++ Powershell/windows/checkbackupstatus.ps1
@@ -3,7 +3,14 @@ $todaydate = get-date -Format "yymmdd"
 $servername = "NATVPN"
 $backuppath = "E:\Config-file\server-config\NATVPN\$todaydate"
 set-Item WSMan:\localhost\Client\TrustedHosts -Value solar.ficld.ir -Force
+$todaydate2= Get-Date -Format "M-d-yyyy"
+$backuppathtor309 = "E:\Config-file\SW1-3064-TOR309\$todaydate2\"
 
 Invoke-Command -ComputerName solar.ficld.ir -Credential $credserver -ScriptBlock {
-    (Get-ChildItem -Path $backuppath).count
+    Backup from SW1-3064-TOR309
+    if ((Get-ChildItem -Path  $($using:backuppathtor309).Count -eq 0) {
+             Write-Output "Message.tor: successfull"
+             Write-Output "Statistic.tor: 1"
+             $status = "up"
+    }  
 }
