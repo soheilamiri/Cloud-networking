6a5cc4752693a3fe7756a06fa39cc706f944b460
soheil <soheil.amiri@Live.com>
2024-08-03 12:46:34 +0330

    update windows post installation task

--- Powershell/windows/windows-Post-installation task.ps1
+++ Powershell/windows/windows-Post-installation task.ps1
@@ -1,3 +1,15 @@
+<# host have no internal access
+1- Network
+    1.1- Allow ICMP
+2- install Tools
+    2.1-install powershell
+    2.2- install choco
+3- Apply OS configuration
+    3.1- set time
+    3.2- rename computer
+    3.3- Apply RRS config
+
+#>
 #Server parameter INPUT
 $servername="ent-AD-1"
 $remotehosts="172.29.249.218"
@@ -10,38 +22,22 @@ set-Item WSMan:\localhost\Client\TrustedHosts -Value $remotehosts -force
 $remotsession=New-PSSession -ComputerName $remotehosts -Credential $Creds -Name rtunnel
 #copy files to remote host
 Copy-Item "E:\Software\05- cloud\02- microsoft\PowerShell-7.4.3-win-x64.msi" -Destination "C:\Users\Administrator\AppData\Local\Temp\" -ToSession $remotsession
-
-Invoke-Command -Session $remotsession -ScriptBlock {
-    Write-Output "start configuration on $($env:COMPUTERNAME):"
-    function postconfig-test
-    {   
-        #from online source
-        #Invoke-WebRequest -Uri https://aka.ms/install-powershell.ps1 -OutFile install-powershell.ps1; ./install-powershell.ps1 -verbose
-        
-
-    }
-    postconfig-test
-}
-
 $remotsession=New-PSSession -ComputerName $remotehosts -Credential $Creds
+
+#1- network config
 Invoke-Command -Session $remotsession -ScriptBlock {
     Write-Output "start configuration on $($env:COMPUTERNAME)"
-    function postconfig-os
+    function postconfig-network
     {
         New-NetFirewallRule -Name "NOC-Allow-ICMP" -DisplayName "NOC-Allow-ICMP" -Description "by-NOC-Orchestrator-echosystem" -Protocol icmpv4 -IcmpType 8 -Enabled True -Profile Domain,Private,Public -Action allow 
-        Set-TimeZone -Name "Iran Standard Time"
-        rename-Computer -NewName $($using:servername) -Force
-        Start-Sleep -Seconds 10
-        Write-Output "$($env:COMPUTERNAME) is going to be restart in 20 sec."
-        Restart-Computer -Force
+        Get-NetAdapterRss -Name ether* | Enable-NetAdapterRss    
     }
-    postconfig-os
+    postconfig-network
 }
+#install Tools
 Invoke-Command -Session $remotsession -ScriptBlock {
     Write-Output "start configuration on $($env:COMPUTERNAME)"
-    function postconfig-initapp
-    {   
-        #install Tools
+    function postconfig-tools
         msiexec.exe C:\Users\Administrator\AppData\Local\Temp\PowerShell-7.4.3-win-x64.msi /quiet ENABLE_PSREMOTING=1
         #Invoke-WebRequest -Uri https://aka.ms/install-powershell.ps1 -OutFile install-powershell.ps1; ./install-powershell.ps1
         #cd c:\Users\Administrator\AppData\Local\Microsoft\powershell\
@@ -53,29 +49,24 @@ Invoke-Command -Session $remotsession -ScriptBlock {
         Write-Output "$($env:COMPUTERNAME) will go to check online update repository."
         choco install winget vim ntop.portable -y --no-progress
     }
-    postconfig-initapp
+    postconfig-tools
     }
+
 
     Invoke-Command -Session $remotsession -ScriptBlock {
         Write-Output "start configuration on $($env:COMPUTERNAME)"
-        function postconfig-tunning
-        {   
-            #install Tools
-            Get-NetAdapterRss -Name ether* | Enable-NetAdapterRss
-            
+        function postconfig-os
+        {
+            Set-TimeZone -Name "Iran Standard Time"
+            rename-Computer -NewName $($using:servername) -Force
+            Start-Sleep -Seconds 10
+            Write-Output "$($env:COMPUTERNAME) is going to be restart in 20 sec."
+            Restart-Computer -Force
         }
-        postconfig-tunning
-        }
-    
-
-    Invoke-Command -Session $remotsession -ScriptBlock {
-        Install-Module -Name PSWindowsUpdate -Force
-    } 
+        postconfig-os
+    }
+ 
 
 Disconnect-PSSession -Session $remotsession
 Remove-PSSession -Session $remotsession
 Get-PSSession
-
-
-$rserver=172.116.144.18
-$rCommand=Get-date
