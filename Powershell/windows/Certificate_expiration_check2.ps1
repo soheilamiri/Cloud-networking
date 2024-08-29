e7f9556fe7a901be8faa586a1c3ff7667c6eee8b
soheil <soheil.amiri@Live.com>
2024-08-29 08:14:47 +0330

    add new Cert check

---
+++ Powershell/windows/Certificate_expiration_check2.ps1
@@ -0,0 +1,54 @@
+# Example: .\Certificate_expiration_check.ps1 chat.lalavij.ir | fl
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $filepath,
+
+    [int]
+    $Port = 443
+)
+# Read each line (URL) from the file
+$Urls = Get-Content -Path $FilePath
+foreach ($ComputerName in $Urls) {
+
+$Certificate = $null
+$TcpClient = New-Object -TypeName System.Net.Sockets.TcpClient
+try {
+
+    $TcpClient.Connect($ComputerName, $Port)
+    $TcpStream = $TcpClient.GetStream()
+
+    $Callback = { param($sender, $cert, $chain, $errors) return $true }
+
+    $SslStream = New-Object -TypeName System.Net.Security.SslStream -ArgumentList @($TcpStream, $true, $Callback)
+    try {
+
+        $SslStream.AuthenticateAsClient('')
+        $Certificate = $SslStream.RemoteCertificate
+
+    } finally {
+        $SslStream.Dispose()
+    }
+
+} finally {
+    $TcpClient.Dispose()
+}
+
+if ($Certificate) {
+    if ($Certificate -isnot [System.Security.Cryptography.X509Certificates.X509Certificate2]) {
+        $Certificate = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList $Certificate
+    }
+
+    #Write-Output $Certificate 
+    #Write-Output $Certificate.NotAfter
+}
+
+
+#days difference between two dates
+Write-Output $ComputerName
+$edate = $Certificate.NotAfter
+$sdate = get-date -Format MM/dd/yyyy
+$timediff = New-TimeSpan -Start $sdate -End $edate
+write-output $timediff.Days
+}
